869fbc4f8e535fe791a1f32ee29d4d186f1b2d0d
jeremy <jeremyhuacs@gmail.com>
2023-08-03 11:27:03 +0800

    delete and update question

--- back-end/app/api/question.py
+++ back-end/app/api/question.py
@@ -3,6 +3,7 @@ from flask import request, jsonify
 from app import db
 from app.model.models import Question, Answer
 from app.utils import util
+from sqlalchemy import exc
 
 @bp.route('/', methods = ('GET',))
 def getQuestions():
@@ -50,3 +51,70 @@ def getOneQuestion(id):
         answers = list(map(lambda answer: dict(answer._mapping), answers))
         question['answers'] = answers
         return jsonify(question)
+
+@bp.route('/create', methods = ('POST',))
+def createQuestion():
+    request_data = request.get_json()
+
+    try:
+        question, answers = util.checkQuestion(request_data)
+    except Exception as e:
+        return e.args
+
+    try:
+        db.session.add(question)
+    except exc.SQLAlchemyError:
+        return 'failed to add questions', 400
+
+    db.session.flush()
+    question_id = question.id
+   
+    for answer in answers:
+        answer.question_id = question_id
+        try:  
+            db.session.add(answer)
+        except exc.SQLAlchemyError:
+            return 'failed to add questions', 400
+    
+    db.session.commit()
+
+    return 'Add question success', 200
+
+@bp.route('/delete/<int:id>', methods = ('DELETE',))
+def deleteQuestion(id):
+    db.session.query(Answer).filter(Answer.question_id == id).delete()
+    db.session.query(Question).filter(Question.id == id).delete()
+    db.session.commit()
+    return 'delete success', 200
+
+@bp.route('/update/<int:id>', methods=('POST',))
+def updateQuestion(id):
+    request_data = request.get_json()
+    try:
+        question, answers = util.checkQuestion(request_data, check_id=True)
+    except Exception as e:
+        return e.args
+    
+    try:
+        db.session.query(Question).filter(Question.id == id).update({
+                Question.content: question.content,
+                Question.analysis: question.analysis,
+                Question.type: question.type,
+                Question.level: question.level,
+                Question.point: question.point
+            })
+    except exc.SQLAlchemyError:
+        return 'failed to update questions', 400
+
+    for answer in answers:
+        try:  
+            db.session.query(Answer).filter(Answer.id == answer.id).update({
+                Answer.content: answer.content,
+                Answer.correct: answer.correct,
+            })
+        except exc.SQLAlchemyError:
+            return 'failed to add questions', 400
+    
+    db.session.commit()
+
+    return 'update success', 200
--- back-end/app/utils/util.py
+++ back-end/app/utils/util.py
@@ -1,8 +1,98 @@
-from app.model.models import Question
-from app.model.enums import QuestionLevel, QuestionType
+from app.model.models import Question, Answer
+from app.model.enums import QuestionLevel, QuestionType, AnswerCorrect
 
 def convertQuestion(question):
     question = dict(question._mapping)
     question['type'] = question['type'].name
     question['level'] = question['level'].name
     return question
+
+def checkQuestion(request_data, check_id = False):
+    try: 
+        content = request_data['content']
+        analysis = request_data['analysis']
+        type = request_data['type']
+        level = request_data['level']
+        point = request_data['point']
+        if check_id == True:
+            question_id = request_data['id']
+    except KeyError:
+        raise Exception('Question not complete', 400)
+
+    if None not in (content, analysis, type, level, point):
+        pass
+    else:
+        raise Exception('Question not complete', 400)
+
+    if check_id == True:
+        if question_id is not None:
+            pass
+        else:
+            raise Exception('Question not complete', 400)
+
+    try:
+        type = QuestionType[type] 
+    except KeyError:
+        raise Exception('Question type is wrong', 400)
+    
+    try: 
+        level = QuestionLevel[level]
+    except KeyError:
+        raise Exception('Question level is wrong', 400)
+
+    question = Question(
+        content = content,
+        analysis = analysis,
+        type = type,
+        level = level,
+        point = point
+    )
+    if check_id == True:
+        question.id = question_id
+
+    request_answers = request_data['answers']
+
+    if len(request_answers) == 0:
+        raise Exception('answers can not be empty', 400)
+
+    correct_values = [member.value for member in AnswerCorrect]
+    for answer in request_answers:
+        try:
+            answer_content = answer['content'] 
+            answer_correct = answer['correct']
+            if check_id == True:
+                answer_id = answer['id']
+        except KeyError:
+            raise Exception('incomplete answers', 400)
+        
+        if None not in (answer_content, answer_correct):
+            pass
+        else:
+            raise Exception('incomplete answers', 400)
+        
+        if answer_id is not None:
+            pass
+        else: 
+            raise Exception('incomplete answers', 400)
+        
+        if answer_correct in correct_values:
+            pass
+        else: 
+            raise Exception('correct field can only be 0 or 1', 400)
+    
+    answers = []
+    for answer in request_answers:
+        if check_id == True:
+            answers.append(Answer(
+                content = answer['content'],
+                correct = answer['correct'],
+                id = answer['id']
+            ))
+        else:
+            answers.append(Answer(
+                content = answer['content'],
+                correct = answer['correct']
+            ))
+    
+    return question, answers
+     
