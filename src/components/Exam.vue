72e09752cc97ad6967ef09d38944137bd019cee7
jeremy <jeremyhuacs@gmail.com>
2023-08-17 09:31:49 +0800

    take exam

--- src/components/Exam.vue
+++ src/components/Exam.vue
@@ -1,54 +1,11 @@
 <template>
   <div class="exam">
 
-    <!-- 表单 -->
-    <el-dialog title="Exam" :visible.sync="dialogFormVisible" append-to-body>
-      <el-form :model="form" label-position="right">
-        <template v-for="(question, index_q) in textPaper">
-          <p>{{ question.content }}</p>
-          <div v-show="question.type == 'singleChoice' || question.type == 'trueOrFalse'">
-            <el-radio-group v-model="form.questions[index_q].answer">
-              <el-radio v-for="answer in question.answers" :label="answer.id.toString()">{{ answer.content }}</el-radio>
-            </el-radio-group>
-          </div>
-
-          <div v-show="question.type == 'shortAnswer'">
-            <el-input v-model="form.questions[index_q].answer" placeholder="请做答"></el-input>
-          </div>
-          <!-- <ul v-for="(answer, a_id) in question.answers" v-show="question.type == 'singleChoice'">
-            <li>{{ answer.content }}</li>
-          </ul>
-          <el-radio-group v-model="form.questions[q_id].answer" v-show="question.type == 'singleChoice'">
-            <el-radio :label=String(question.answers[0].id)>A</el-radio>
-            <el-radio :label=String((question.answers[0].id) + 1)>B</el-radio>
-            <el-radio :label=String((question.answers[0].id) + 2)>C</el-radio>
-            <el-radio :label=String((question.answers[0].id) + 3)>D</el-radio>
-          </el-radio-group>
-          <el-radio-group v-model="form.questions[q_id].answer" v-show="question.type == 'trueOrFalse'">
-            <el-radio :label=String(question.answers[0].id)>true</el-radio>
-            <el-radio :label=String((question.answers[0].id) + 1)>false</el-radio>
-          </el-radio-group>
-          <el-form-question label="" prop="" v-show="question.type == 'shortAnswer'">
-            <el-input v-model="form.exam_id" autocomplete="off" placeholder=""></el-input>
-          </el-form-question>
-          <el-divider> </el-divider> -->
-        </template>
-      </el-form>
-      <div slot="footer" class="dialog-footer">
-        <el-button type="primary" @click="submitForm">交 卷</el-button>
-      </div>
-    </el-dialog>
-
     <!-- slice(a,b)的作用是从已有的数组中返回选定的元素"a"表示开始，"b"表示结束。 -->
-    <el-table :data="tableData.slice((currentPage - 1) * pageSize, currentPage * pageSize)" @row-click="handle" style="width: 100%;
+    <el-table :data="tableData.slice((currentPage - 1) * pageSize, currentPage * pageSize)" style="width: 100%;
       margin: auto;
       margin-top: 20px;
       box-shadow: 0px 5px 10px rgba(0, 0, 0, .12), 0px 5px 10px rgba(0, 0, 0, .04); " max-height="500">
-
-      <!--
-      <el-table-column fixed prop="id" label="试卷编号" width="100">
-      </el-table-column>
-      -->
 
       <el-table-column prop="testpaper_name" label="试卷名称" width="120">
       </el-table-column>
@@ -59,7 +16,7 @@
       </el-table-column>
       <el-table-column fixed="right" label="操作" width="120">
         <template slot-scope="scope">
-          <el-button type="primary" size="medium" @click="open">
+          <el-button type="primary" size="medium" @click="take(scope.row.testpaper_id)">
             参加考试
           </el-button>
         </template>
@@ -85,15 +42,7 @@ export default {
   name: 'exam',
   data() {
     return {
-      id: '', // 试卷id
       tableData: [], // 所有考试信息
-      textPaper: [], // 考试试卷
-      radio: {}, // 记录考生作答情况
-      form: {
-        exam_id: '',
-        questions: []
-      },
-      dialogFormVisible: false,
       currentPage: 1, // 当前页码
       pageSize: 5 // 每页的数据条数
     }
@@ -109,65 +58,13 @@ export default {
         console.log(res);//发生错误时执行的代码
       })
     },
-
-    //获取某份试卷信息
-    getTextPaper(id) {
-      // const token = cookie.get('jwt')
-      axios.get('/paper/' + id).then(res => {
-        this.textPaper = res.data.questions;
-        this.dialogFormVisible = true;
-        // this.tableData = res.data;//将后台传递的数组赋值给定义的空数组
-        this.form.exam_id = res.data.id; //给form表单初始化
-        this.form.questions = []
-
-        for (let i = 0; i < this.textPaper.length; i++) {
-          let question = this.textPaper[i]
-          this.form.questions.push({
-            'question_id': question.id,
-            'answer': '',
-            'type': question.type
-          })
-        }
-      }).catch(res => {
-        console.log("异常触发");
-        console.log(res); //发生错误时执行的代码
-      })
-    },
-
-    postTextPaper() {
-      const token = cookie.get('jwt')
-      axios.post('/wrong/myExam', this.form, { headers: { 'Authorization': token } }).then(res => {
-      }).catch(res => {
-        console.log("异常触发");
-        console.log(res); //发生错误时执行的代码
-      })
-    },
-
-    submitForm() {
-      console.log(this.form);
-      this.$confirm('确认交卷?', '提示', {
-        confirmButtonText: '是',
-        cancelButtonText: '否',
-        type: 'warning'
-      }).then(() => {
-        this.postTextPaper();
-        this.dialogFormVisible = false
-      }).catch(() => {
-        alert('已取消交卷!');
-      })
-    },
-
-    handle(row) {
-      this.id = row.testpaper_id;
-    },
-
-    open() {
+    take(id) {
       this.$confirm('是否现在参加考试?', '提示', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
         type: 'warning'
       }).then(() => {
-        this.getTextPaper(this.id);
+        this.$router.push({name: 'takeExam', params: {'exam_id': id}})
       }).catch(() => {
         // alert('已取消删除!');
       });
